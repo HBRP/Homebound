bfd080ccdafd1afb0b32225d4f7c69b9987bdcba
Emma Davenport <davenport.physics@gmail.com>
2021-03-12 13:21:50 -0600

    Remove jobs, added groups and Comamnd schema

--- sql/SeedTable.sql
+++ sql/SeedTable.sql
@@ -10,32 +10,57 @@ VALUES
     ('Misc');
 
 INSERT INTO
-    Job.Field (JobFieldName)
+    Groups.Types (GroupTypeName)
 VALUES
-    ('Unemployed'),
-    ('Department of Justice'),
-    ('Los Santos Police Department'),
-    ('Blaine County Sheriffs Department'),
-    ('U.S. Marshals Service');
+    ('Job'),
+    ('Group'),
+    ('Misc');
+
+INSERT INTO
+    Groups.Groups (GroupName, GroupTypeId)
+VALUES
+    ('Unemployed', 3),
+    ('Department of Justice', 1),
+    ('Los Santos Police Department', 1),
+    ('Blaine County Sheriffs Department', 1),
+    ('U.S. Marshals Service', 1);
 
 INSERT INTO 
-    Job.Type (JobFieldId, Rank, Pay, Paygrade) 
+    Groups.Rank (GroupId, Rank) 
 VALUES
-    (1, 'Unemployed', 0, 0),
-    (2, 'Chief Justice', 100, 100),
-    (2, 'Justice', 90, 90),
-    (2, 'Judge', 80, 80),
-    (2, 'Chief Public Defender', 70, 70),
-    (2, 'Asst. Chief Public Defender', 60, 60),
-    (2, 'Public Defender', 50, 50),
-    (2, 'District Attorney', 70, 70),
-    (2, 'Asst. District Attorney', 60, 60),
-    (2, 'Prosecutor', 50, 50),
-    (2, 'Paralegal', 40, 40),
-    (5, 'US Marshal', 80, 100),
-    (5, 'Chief Deputy US Marshal', 70, 90),
-    (5, 'Supervisory US Marshal', 60, 80),
-    (5, 'Deputy US Marshal', 50, 70);
+    (1, 'Unemployed'),
+    (2, 'Chief Justice'),
+    (2, 'Justice'),
+    (2, 'Judge'),
+    (2, 'Chief Public Defender'),
+    (2, 'Asst. Chief Public Defender'),
+    (2, 'Public Defender'),
+    (2, 'District Attorney'),
+    (2, 'Asst. District Attorney'),
+    (2, 'Prosecutor'),
+    (2, 'Paralegal'),
+    (5, 'US Marshal'),
+    (5, 'Chief Deputy US Marshal'),
+    (5, 'Supervisory US Marshal'),
+    (5, 'Deputy US Marshal');
+
+INSERT INTO
+    Groups.JobPay (GroupRankId, Pay)
+VALUES 
+    (2, 100),
+    (3, 90),
+    (4, 80),
+    (5, 70),
+    (6, 60),
+    (7, 50),
+    (8, 70),
+    (9, 60),
+    (10, 50),
+    (11, 15),
+    (12, 80),
+    (13, 70),
+    (14, 60),
+    (15, 50);
 
 INSERT INTO
     Player.Types (PlayerType)
@@ -45,13 +70,6 @@ VALUES
     ('Dev'),
     ('Admin'),
     ('Owner');
-
-INSERT INTO
-    Player.TypePermissions (PersonTypeId, Permission)
-VALUES
-    (1, 'SomeCommand'),
-    (1, 'SomeCommand'),
-    (1, 'SomeOtherCommand');
 
 INSERT INTO
     Storage.Types (StorageTypeName, StorageTypeSlots)
@@ -98,3 +116,11 @@ VALUES
     (102, 84, 'Platic Surgery Unit', -676.7, 311.5, 82.5),       -- Eclipse Medical Tower
     (102, 84, 'Platic Surgery Unit', -449.8, -341.0, 33.7),      -- Mount Zonah Medical Center
     (102, 84, 'Platic Surgery Unit', 298.7, -584.6, 42.2);       -- Pillbox Hill Medical Center
+
+
+INSERT INTO 
+    Command.PlayerTypePermissions (PlayerTypeId, Permission)
+VALUES
+    (1, 'me'),
+    (1, 'looc'),
+    (1, 'ad');
