0dfd91f37d201313cd2c2cdf125dfd66a07c44d8
Emma Davenport <davenport.physics@gmail.com>
2021-03-14 12:20:56 -0500

    Fixed cops and cop cars not spawning. Removed ems cops from spawning in certain areas

--- resources/internal/em_no_emergency/client/c_no_emergency.lua
+++ resources/internal/em_no_emergency/client/c_no_emergency.lua
@@ -3,7 +3,7 @@
 local function clear_local_area_of_cops()
 
     local coords = GetEntityCoords(PlayerPedId())
-    ClearAreaOfCops(coords.x, coords.y, coords.z , 1000, 0)
+    ClearAreaOfCops(coords.x, coords.y, coords.z , 1000.0)
 
 end
 
@@ -15,11 +15,17 @@ local function disable_dispatch_service()
 
 end
 
+
 local function remove_vehicles_from_areas()
 
-    RemoveVehiclesFromGeneratorsInArea(412.174, -1012.538, 32.0, 405.46, -975.148, 27.0) -- Mission Row Front Parking
+    RemoveVehiclesFromGeneratorsInArea(-448.381, -328.73, 34.50, -499.502, -346.43, 34.50) -- Mount Zonah
+    RemoveVehiclesFromGeneratorsInArea(-629.365, -98.2345, 38.065, -643.37, -115.4596, 38.06) -- Government Center Fire department (DOES NOT WORK)
+    RemoveVehiclesFromGeneratorsInArea(291.604, -566.823, 43.2608, 288.7409, -614.184, 43.422) -- Pillbox Medical
+    RemoveVehiclesFromGeneratorsInArea(1837.8359, -3697.2387, 34.207, 1802.4299, 3682.012, 34.224) -- Sandy Medical
+    RemoveVehiclesFromGeneratorsInArea(339.08, -1341.817, 32.4525, 326.145, -1491.701, 32.4525) -- Crusade Hospital (DOES NOT WORK)
 
 end
+
 
 local function never_wanted()
     SetPlayerWantedLevel(PlayerId(), 0, false)
@@ -29,9 +35,8 @@ end
 
 Citizen.CreateThread(function()
 
-    while true do
-        Citizen.Wait(500)
-    end
+    Citizen.Wait(0)
+    disable_dispatch_service()
 
 end)
 
@@ -40,7 +45,6 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
         clear_local_area_of_cops()
-        disable_dispatch_service()
         never_wanted()
         remove_vehicles_from_areas()
     end
