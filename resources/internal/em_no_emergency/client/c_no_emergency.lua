891fa151539e9f45842b3233e0a8511a2a4b3919
Emma Davenport <davenport.physics@gmail.com>
2021-03-14 12:40:06 -0500

    Removed lower pillbox vehicles

--- resources/internal/em_no_emergency/client/c_no_emergency.lua
+++ resources/internal/em_no_emergency/client/c_no_emergency.lua
@@ -21,6 +21,7 @@ local function remove_vehicles_from_areas()
     RemoveVehiclesFromGeneratorsInArea(-448.381, -328.73, 34.50, -499.502, -346.43, 34.50) -- Mount Zonah
     RemoveVehiclesFromGeneratorsInArea(-629.365, -98.2345, 38.065, -643.37, -115.4596, 38.06) -- Government Center Fire department (DOES NOT WORK)
     RemoveVehiclesFromGeneratorsInArea(291.604, -566.823, 43.2608, 288.7409, -614.184, 43.422) -- Pillbox Medical
+    RemoveVehiclesFromGeneratorsInArea(378.5925, -575.367, 28.839, 354.327, -600.7609, 28.77) -- Pillbox Medical Lower
     RemoveVehiclesFromGeneratorsInArea(1837.8359, -3697.2387, 34.207, 1802.4299, 3682.012, 34.224) -- Sandy Medical
     RemoveVehiclesFromGeneratorsInArea(339.08, -1341.817, 32.4525, 326.145, -1491.701, 32.4525) -- Crusade Hospital (DOES NOT WORK)
 
@@ -37,6 +38,10 @@ Citizen.CreateThread(function()
 
     Citizen.Wait(0)
     disable_dispatch_service()
+    while true do
+        Citizen.Wait(200)
+        remove_vehicles_from_areas()
+    end
 
 end)
 
@@ -46,7 +51,6 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
         clear_local_area_of_cops()
         never_wanted()
-        remove_vehicles_from_areas()
     end
 
 end)
