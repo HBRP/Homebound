481923e3bb9caf3a3adf6032d00d67234e0d407b
Emma Davenport <davenport.physics@gmail.com>
2021-03-21 11:12:18 -0500

    Item loops are not dependant on spawning

--- resources/internal/em_storage/client/c_storage.lua
+++ resources/internal/em_storage/client/c_storage.lua
@@ -3,12 +3,11 @@ local nearby_stashes = {}
 
 local function stash_loop()
 
-    local ped = PlayerPedId()
     local triggered_ui = false
     while true do
 
         Citizen.Wait(5)
-        local ped_coords = GetEntityCoords(ped)
+        local ped_coords = GetEntityCoords(PlayerPedId())
         local nearby_a_stash = false
         for i = 1, #nearby_stashes do
             if GetDistanceBetweenCoords(ped_coords.x, ped_coords.y, ped_coords.z, nearby_stashes[i].x, nearby_stashes[i].y, nearby_stashes[i].z, true) < 5 then
@@ -18,6 +17,7 @@ local function stash_loop()
                 if IsControlJustReleased(0, 38) then
                     TriggerEvent("esx_inventoryhud:open_secondary_inventory", nearby_stashes[i].storage_id)
                     TriggerEvent('cd_drawtextui:HideUI')
+                    Citizen.Wait(1000)
                 end
             end
         end
@@ -25,7 +25,7 @@ local function stash_loop()
             if triggered_ui then
                 TriggerEvent('cd_drawtextui:HideUI')
             end
-            Citizen.Wait(200)
+            Citizen.Wait(500)
         end
     end
 
@@ -42,9 +42,5 @@ local function stash_refresh_loop()
 
 end
 
-AddEventHandler("em_fw:character_loaded", function()
-
-    Citizen.CreateThread(stash_loop)
-    Citizen.CreateThread(stash_refresh_loop)
-
-end)
+Citizen.CreateThread(stash_loop)
+Citizen.CreateThread(stash_refresh_loop)
