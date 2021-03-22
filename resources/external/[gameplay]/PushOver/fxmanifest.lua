8ed90722062c46fcf240ca6b71fcaa1ea1208c55
Emma Davenport <davenport.physics@gmail.com>
2021-03-22 10:29:05 -0500

    Addition of heavily modifed PushOver script

---
+++ resources/external/[gameplay]/PushOver/fxmanifest.lua
@@ -0,0 +1,21 @@
+-----------------------------------------------------------
+-- PushOver- A Simple FiveM Script, Made By Jordan.#2139 --
+-----------------------------------------------------------
+
+fx_version "bodacious"
+game "gta5"
+
+-- Define the resource metadata
+name "PushOver"
+description "A simple non-ESX script to push cars"
+author "Jordan.#2139"
+version "v1.0.0"
+
+
+client_script "config.lua"
+client_script "client.lua"
+
+dependencies {
+    'em_commands',
+    't-notify'
+}
