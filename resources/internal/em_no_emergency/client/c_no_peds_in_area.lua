58ee2c7bd5fa10abdcfcfc56fcb461268ac38c2e
Emma Davenport <davenport.physics@gmail.com>
2021-03-21 20:22:29 -0500

    Prevent peds from spawning at mission row

---
+++ resources/internal/em_no_emergency/client/c_no_peds_in_area.lua
@@ -0,0 +1,24 @@
+
+local area_locations = {
+    {x = 448.445, y = -994.68, z = 29.38, radius = 60} -- mission row
+}
+
+local function clear_area_of_peds()
+
+    local coords = GetEntityCoords(PlayerPedId())
+    for i = 1, #area_locations do
+        ClearAreaOfPeds(area_locations[i].x, area_locations[i].y, area_locations[i].z, area_locations[i].radius, 1)
+    end
+
+end
+
+
+
+Citizen.CreateThread(function()
+
+    while true do
+        Citizen.Wait(100)
+        clear_area_of_peds()
+    end
+
+end)
--- resources/internal/em_no_emergency/fxmanifest.lua
+++ resources/internal/em_no_emergency/fxmanifest.lua
@@ -5,6 +5,7 @@ games { 'gta5' }
 client_scripts {
 
     'client/c_no_emergency.lua',
-    'client/c_no_ped_weapon_drops.lua'
+    'client/c_no_ped_weapon_drops.lua',
+    'client/c_no_peds_in_area.lua'
 
 }
