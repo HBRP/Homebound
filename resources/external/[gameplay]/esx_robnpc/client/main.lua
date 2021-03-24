6c7e8376f169f84b6feb30bd832ae3d3bfc42fee
Emma Davenport <davenport.physics@gmail.com>
2021-03-24 16:38:08 -0500

    Robbing resource now gives no indication of robbery starting

--- resources/external/[gameplay]/esx_robnpc/client/main.lua
+++ resources/external/[gameplay]/esx_robnpc/client/main.lua
@@ -6,6 +6,7 @@ Citizen.CreateThread(function()
 
         local aiming, targetPed = GetEntityPlayerIsFreeAimingAt(PlayerId(-1))
         if aiming then
+
             TaskSetBlockingOfNonTemporaryEvents(targetPed)
             if IsControlJustPressed(0, 38) then
                 local playerPed = GetPlayerPed(-1)
@@ -14,11 +15,11 @@ Citizen.CreateThread(function()
 
                 if DoesEntityExist(targetPed) and IsEntityAPed(targetPed) then
                     if robbedRecently then
-                        exports['swt_notifications']:Negative("Rob Npc", 'Cannot rob another person yet', "top", 2000, true)
+                        exports['swt_notifications']:Negative("Robbing", 'Cannot rob another person yet', "top", 2000, true)
                     elseif IsPedDeadOrDying(targetPed, true) then
-                        exports['swt_notifications']:Negative("Rob Npc", 'Target dead', "top", 2000, true)
+                        exports['swt_notifications']:Negative("Robbing", 'Target dead', "top", 2000, true)
                     elseif GetDistanceBetweenCoords(pCoords.x, pCoords.y, pCoords.z, tCoords.x, tCoords.y, tCoords.z, true) >= Config.RobDistance then
-                        exports['swt_notifications']:Negative("Rob Npc", 'Target too far away', "top", 2000, true)
+                        exports['swt_notifications']:Negative("Robbing", 'Target too far away', "top", 2000, true)
                     else
                         robNpc(targetPed)
                     end
@@ -42,7 +43,6 @@ function robNpc(targetPed)
         TaskStandStill(targetPed, Config.RobAnimationSeconds * 1000)
         FreezeEntityPosition(targetPed, true)
         TaskPlayAnim(targetPed, dict, 'handsup_standing_base', 8.0, -8, .01, 49, 0, 0, 0, 0)
-        exports['swt_notifications']:Info("Rob Npc", 'Robbery started', "top", 2000, true)
 
         exports["rprogress"]:Custom({
             Async    = false,
@@ -50,13 +50,14 @@ function robNpc(targetPed)
             Label = "Robbing"
         })
 
-        exports["em_fw"]:give_item(exports["em_fw"]:get_character_storage_id(), exports["em_items"]:get_item_id_from_name("cash"), math.random(Config.MinMoney, Config.MaxMoney), -1, -1)
+        local amount = math.random(Config.MinMoney, Config.MaxMoney)
+        exports["em_fw"]:give_item(exports["em_fw"]:get_character_storage_id(), exports["em_items"]:get_item_id_from_name("cash"), amount , -1, -1)
         FreezeEntityPosition(targetPed, false)
-        exports['swt_notifications']:Success("Rob Npc", 'Robbery complete', "top", 2000, true)
+        exports['swt_notifications']:Success("Robbing", string.format("Stole $%d", amount), "top", 2000, true)
 
         if Config.ShouldWaitBetweenRobbing then
             Citizen.Wait(math.random(Config.MinWaitSeconds, Config.MaxWaitSeconds) * 1000)
-            exports['swt_notifications']:Info("Rob Npc", 'Can rob again', "top", 2000, true)
+            exports['swt_notifications']:Info("Robbing", 'Can rob again', "top", 2000, true)
         end
         ClearPedTasksImmediately(targetPed)
 
