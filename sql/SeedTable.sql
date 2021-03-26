91813b473b14b107e2af75d59ad116b1d66f9d38
Emma Davenport <davenport.physics@gmail.com>
2021-03-25 19:22:21 -0500

    Addition of Clothing store stores

--- sql/SeedTable.sql
+++ sql/SeedTable.sql
@@ -123,7 +123,28 @@ INSERT INTO
 VALUES
     ('Convenience Store'),
     ('Black Market'),
-    ('Pawn Shop');
+    ('Pawn Shop'),
+    ('Clothing'),
+    ('Barber'),
+    ('Surgery');
+
+insert into 
+    store.stores (StoreTypeId, StoreName, X, Y, Z)
+VALUES
+    (4, 'Clothing Store', 72.3, -1399.1, 28.4),
+    (4, 'Clothing Store', -703.8, -152.3, 36.4),
+    (4, 'Clothing Store', -167.9, -299.0, 38.7),
+    (4, 'Clothing Store', 428.7, -800.1, 28.5),
+    (4, 'Clothing Store', -829.4, -1073.7, 10.3),
+    (4, 'Clothing Store', -1447.8, -242.5, 48.8),
+    (4, 'Clothing Store', 11.6, 6514.2, 30.9),
+    (4, 'Clothing Store', 123.6, -219.4, 53.6),
+    (4, 'Clothing Store', 1696.3, 4829.3, 41.1),
+    (4, 'Clothing Store', 618.1, 2759.6, 41.1),
+    (4, 'Clothing Store', 1190.6, 2713.4, 37.2),
+    (4, 'Clothing Store', -1193.4, -772.3, 16.3),
+    (4, 'Clothing Store', -3172.5, 1048.1, 19.9),
+    (4, 'Clothing Store', -1108.4, 2708.9, 18.1);
 
 INSERT INTO Blip.Blips
     (BlipType, BlipColor, BlipName, X, Y, Z, Static)
