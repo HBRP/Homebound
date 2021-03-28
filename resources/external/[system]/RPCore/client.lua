42dcea2bd35fbfac28dce9e050df7951afdb67e0
Emma Davenport <davenport.physics@gmail.com>
2021-03-28 18:49:02 -0500

    Change: reticle no longer removed

--- resources/external/[system]/RPCore/client.lua
+++ resources/external/[system]/RPCore/client.lua
@@ -25,44 +25,6 @@ if Config.pvp then
     end)
 end
 
-if Config.noreticle then
-    local scopedWeapons = 
-    {
-        100416529,  -- WEAPON_SNIPERRIFLE
-        205991906,  -- WEAPON_HEAVYSNIPER
-        3342088282  -- WEAPON_MARKSMANRIFLE
-    }
-    
-    function HashInTable( hash )
-        for k, v in pairs( scopedWeapons ) do 
-            if ( hash == v ) then 
-                return true 
-            end 
-        end 
-    
-        return false 
-    end 
-    
-    function ManageReticle()
-        local ped = PlayerPedId()
-    
-        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then
-            local _, hash = GetCurrentPedWeapon( ped, true )
-    
-            if ( GetFollowPedCamViewMode() ~= 4 and IsPlayerFreeAiming() and not HashInTable( hash ) ) then 
-                HideHudComponentThisFrame( 14 )
-            end 
-        end 
-    end 
-    
-    Citizen.CreateThread( function()
-        while true do 
-            HideHudComponentThisFrame( 14 )     
-            Citizen.Wait(0)
-        end 
-    end)
-end
-
 if Config.crouch then
     local crouched = false
     
--- resources/external/[system]/RPCore/config.lua
+++ resources/external/[system]/RPCore/config.lua
@@ -3,7 +3,6 @@ Config = {}
 Config.deleteveh = true -- Delete vehicle command (/dv)
 Config.handsup = true -- Press x to put your hands up
 Config.crouch = true -- Pressing ctrl makes you crouch
-Config.noreticle = true -- Disables the reticle when aiming
 Config.fingerpoint = true -- Press b to point
 Config.nogrip = true -- When jumping and running, there is an 90% chance of falling (Customizable)
 Config.pvp = true -- Enable pvp
@@ -11,7 +10,7 @@ Config.delallveh = true -- Delete all vehicles on the map
 
 -- Customize the NoGrip script here --
 
-Config.ragdoll_chance = 0.8 -- Edit this decimal value for chance of falling (e.g. 80% = 0.8    75% = 0.75    32% = 0.32)
+Config.ragdoll_chance = 0.5 -- Edit this decimal value for chance of falling (e.g. 80% = 0.8    75% = 0.75    32% = 0.32)
 
 
 
