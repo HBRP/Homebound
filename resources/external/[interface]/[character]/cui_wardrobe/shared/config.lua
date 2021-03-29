d9163ee9daa9eef1fd76d882a1ca1001b188cef5
Emma Davenport <davenport.physics@gmail.com>
2021-03-29 15:19:07 -0500

    Change: Removed blips from cui_wardrobe

--- resources/external/[interface]/[character]/cui_wardrobe/shared/config.lua
+++ resources/external/[interface]/[character]/cui_wardrobe/shared/config.lua
@@ -14,12 +14,6 @@ Config.SlotsNumber = 10
     to be opened by users at the locations defined in the table below.
 --]]
 Config.UseAnywhere = false
-Config.DisplayBlips = true
+Config.DisplayBlips = false
 Config.Locations = {
-    -- default locations
-    vector3(-811.80, 175.10, 76.80),    -- Michael's house wardrobe
-    vector3(8.84, 528.15, 170.81),      -- Franklin's Vinewood house wardrobe
-    vector3(-18.0, -1439.0, 31.2),      -- Franklin's old room
-    vector3(-1153.13, -1516.95, 10.82), -- Floyd's house in La Puerta
-    vector3(-109.71, -11.97, 70.51)     -- Harvey Molina's apartment
 }
