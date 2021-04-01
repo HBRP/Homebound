c090dcd06c2bfd36febcdb7c8f5cde0d6073e28f
Emma Davenport <davenport.physics@gmail.com>
2021-04-01 12:10:59 -0500

    Addition: em_restart for restarting certain annoying resources

---
+++ resources/internal/em_restart/server/s_restart.lua
@@ -0,0 +1,23 @@
+
+local resources = {
+
+    'fivem-appearance',
+    'cui_wardrobe',
+    'em_customization',
+    'em_stores'
+
+}
+
+Citizen.CreateThread(function()
+
+    Citizen.Wait(5000)
+    for i = 1, #resources do
+        ExecuteCommand(string.format("stop %s", resources[i]))
+        Citizen.Wait(100)
+    end
+    for i = 1, #resources do
+        ExecuteCommand(string.format("start %s", resources[i]))
+        Citizen.Wait(100)
+    end
+
+end)
