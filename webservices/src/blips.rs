bdad0fc5ffda03f4e9b211d8237180a8eac3e810
Emma Davenport <davenport.physics@gmail.com>
2021-03-31 21:06:38 -0500

    Addition: get_blip_group_subscriptions endpoint

--- webservices/src/blips.rs
+++ webservices/src/blips.rs
@@ -14,6 +14,15 @@ struct Blip {
 
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct BlipSubscription {
+
+    group_id: i32,
+    blip_color: i32,
+    blip_group_id: i32
+
+}
+
 #[get("/Blip/Get")]
 pub fn get_blips() -> String {
 
@@ -35,5 +44,27 @@ pub fn get_blips() -> String {
     }
 
     serde_json::to_string(&blips).unwrap()
+
+}
+
+#[get("/Blip/GroupSubscriptions")]
+pub fn get_blip_group_subscriptions() -> String {
+
+    let mut client = db_postgres::get_connection().unwrap();
+    let mut blip_subscriptions: Vec<BlipSubscription> = Vec::new();
+
+    for row in client.query("SELECT GroupId, BlipColor, BlipGroupId FROM Groups.BlipSubscription", &[]).unwrap() {
+
+        blip_subscriptions.push(BlipSubscription {
+
+            group_id: row.get("GroupId"),
+            blip_color: row.get("BlipColor"),
+            blip_group_id: row.get("BlipGroupId")
+
+        });
+
+    }
+
+    return serde_json::to_string(&blip_subscriptions).unwrap();
 
 }
