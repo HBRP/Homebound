455ab064d32b0eb7d4c4f67a6a6785840c1c4e5a
Emma Davenport <davenport.physics@gmail.com>
2021-04-02 10:38:13 -0500

    Change: TriggerEvent when opening store

--- resources/external/[interface]/[storage]/esx_inventoryhud/client/main.lua
+++ resources/external/[interface]/[storage]/esx_inventoryhud/client/main.lua
@@ -329,6 +329,7 @@ end)
 
 AddEventHandler("esx_inventoryhud:open_store", function(store, store_items)
 
+    TriggerEvent("opened_inventory")
     SendNUIMessage(
         {
             action = "setType",
