dff04e7a74f4fcb4cd3e3f66d47f48d8bfe37b25
Emma Davenport <davenport.physics@gmail.com>
2021-04-06 12:41:11 -0500

    Addition of command logging

--- resources/internal/em_fw/server/commands/s_commands.lua
+++ resources/internal/em_fw/server/commands/s_commands.lua
@@ -1,7 +1,7 @@
 
-register_server_callback("em_fw:can_use_command", function(source, callback, character_id, command)
+register_server_callback("em_fw:can_use_command", function(source, callback, character_id, command, command_arguments)
 
-    local data = {character_id = character_id, command = command}
+    local data = {character_id = character_id, command = command, command_arguments = command_arguments}
     HttpPost("/Command/CanUseCommand", data, function(error_code, result_data, result_headers)
 
         local temp = json.decode(result_data)
