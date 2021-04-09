5abf83a015895cdd27d8796d0ba184dd54068d50
Emma Davenport <davenport.physics@gmail.com>
2021-04-09 14:15:03 -0500

    Addition of animations resource

---
+++ resources/internal/[system]/em_animations/client/c_animations.lua
@@ -0,0 +1,30 @@
+
+local animation_flags = {
+
+    ANIM_FLAG_NORMAL = 0,
+    ANIM_FLAG_REPEAT = 1,
+    ANIM_FLAG_STOP_LAST_FRAME = 2,
+    ANIM_FLAG_UPPERBODY = 16,
+    ANIM_FLAG_ENABLE_PLAYER_CONTROL = 32,
+    ANIM_FLAG_CANCELABLE = 120
+
+}
+
+
+function play_animation(dict, anim, duration, enum_flags)
+
+    RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        Citizen.Wait(5)
+    end
+
+    TaskPlayAnim(PlayerPedId(), dict, anim, 8.0, 8.0, duration, enum_flags, 1.0, false, false, false)
+
+    Citizen.CreateThread(function()
+
+        Citizen.Wait(duration)
+        ClearPedTasks(ped)
+
+    end)
+
+end
---
+++ resources/internal/[system]/em_animations/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+
+client_scripts {
+
+    'client/c_animations.lua'
+
+}
+
+exports {
+    'play_animation'
+}
