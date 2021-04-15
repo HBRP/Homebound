b52b5d0270fc598a0d7d5cca74339917a0d2ce6c
Emma Davenport <davenport.physics@gmail.com>
2021-04-15 18:33:15 -0500

    Optimized gsr loop

--- resources/internal/[system]/em_police_commands/client/c_police_gsr.lua
+++ resources/internal/[system]/em_police_commands/client/c_police_gsr.lua
@@ -23,6 +23,10 @@ end, "Check if someone recently shot a gun", {{name = "character_id", help = "tr
 RegisterNetEvent("em_police_commands:gsr_request")
 AddEventHandler("em_police_commands:gsr_request", function(other_character_id)
 
+    if disappear_time < GetGameTimer() then
+        has_gsr = false
+    end
+
     exports["em_fw"]:trigger_event_for_character("em_police_commands:gsr_response", other_character_id, has_gsr)
 
 end)
@@ -38,6 +42,7 @@ AddEventHandler("em_police_commands:gsr_response", function(gsr_positive)
 
 end)
 
+--[[
 Citizen.CreateThread(function()
 
     while true do
@@ -54,3 +59,39 @@ Citizen.CreateThread(function()
     end
 
 end)
+]]
+
+local running_gsr_loop = false
+local function gsr_loop()
+
+    print("in loop")
+    local end_time = GetGameTimer() + 1000
+    while end_time > GetGameTimer() do
+
+        Citizen.Wait(20)
+        if exports["em_items"]:is_ped_shooting_a_gun() then
+            has_gsr = true
+            disappear_time = GetGameTimer() + 1000 * 60 * 15
+        end
+
+        if disappear_time < GetGameTimer() then
+            has_gsr = false
+        end
+
+    end
+    running_gsr_loop = false
+
+end
+
+RegisterCommand('shoot_gun', function() 
+
+    if running_gsr_loop then
+        return
+    end
+
+    running_gsr_loop = true
+    Citizen.CreateThread(gsr_loop)
+
+end, false)
+
+RegisterKeyMapping('shoot_gun', 'shoot_gun', 'mouse_button', 'MOUSE_LEFT')
