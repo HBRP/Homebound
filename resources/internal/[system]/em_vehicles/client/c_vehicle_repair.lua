7a916743922058ed1a6488797d3bf51249ddf300
Emma Davenport <davenport.physics@gmail.com>
2021-04-15 17:08:27 -0500

    Repair vehicle now sets engine health correctly

--- resources/internal/[system]/em_vehicles/client/c_vehicle_repair.lua
+++ resources/internal/[system]/em_vehicles/client/c_vehicle_repair.lua
@@ -4,6 +4,9 @@ function repair_vehicle(veh)
 
     SetVehicleFixed(veh)
     SetVehicleEngineHealth(veh, 1000.0)
-    SetVehicleFuelLevel(veh, 65)
+
+    if GetVehicleFuelLevel(veh) < 65.0 then
+        SetVehicleFuelLevel(veh, 65.0)
+    end
 
 end
