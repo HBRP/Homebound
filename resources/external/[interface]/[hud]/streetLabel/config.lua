a52f624169f86f49e547ae2c564956b3be69dc11
Emma Davenport <davenport.physics@gmail.com>
2021-04-15 09:43:31 -0500

    Changed colors on streetlabels

--- resources/external/[interface]/[hud]/streetLabel/config.lua
+++ resources/external/[interface]/[hud]/streetLabel/config.lua
@@ -17,25 +17,24 @@ config = {
 	};
 
 	crossing = {
-		r = 240;
-		g = 220;
-		b = 80;
+		r = 255;
+		g = 255;
+		b = 255;
 		a = 0.9;
 		size = 1;
 	};
 
 	direction = {
-		r = 240;
-		g = 220;
-		b = 80;
+		r = 255;
+		g = 255;
+		b = 255;
 		a = 0.9;
 		size = 2;
 	};
 
 	position = {
-		-- 0:100
-		offsetX = 17;
-		offsetY = 2.5; 
+		offsetX = 15;
+		offsetY = 3.5; 
 	};
 
 	vehicleCheck = true; -- Rather or not to display HUD when player(s) are inside a vehicle
