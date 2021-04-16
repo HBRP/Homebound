6993af85a349418369e3a7b3105e50d3587a6d80
Emma Davenport <davenport.physics@gmail.com>
2021-04-16 09:05:57 -0500

    Removed vehicle generator line for firetruck

--- resources/internal/[system]/em_no_emergency/client/c_no_emergency.lua
+++ resources/internal/[system]/em_no_emergency/client/c_no_emergency.lua
@@ -81,7 +81,6 @@ end
 local function remove_vehicles_from_areas()
 
     RemoveVehiclesFromGeneratorsInArea(-448.381, -328.73, 34.50, -499.502, -346.43, 34.50) -- Mount Zonah
-    RemoveVehiclesFromGeneratorsInArea(-629.365, -98.2345, 38.065, -643.37, -115.4596, 38.06) -- Government Center Fire department (DOES NOT WORK)
     RemoveVehiclesFromGeneratorsInArea(291.604, -566.823, 43.2608, 288.7409, -614.184, 43.422) -- Pillbox Medical
     RemoveVehiclesFromGeneratorsInArea(378.5925, -575.367, 28.839, 354.327, -600.7609, 28.77) -- Pillbox Medical Lower
     RemoveVehiclesFromGeneratorsInArea(1837.8359, -3697.2387, 34.207, 1802.4299, 3682.012, 34.224) -- Sandy Medical
