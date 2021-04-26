10776ac9559c35e30674f80b72d19fea16560594
Emma Davenport <davenport.physics@gmail.com>
2021-04-26 09:39:38 -0500

    Addition of scoreboard resource

---
+++ resources/external/[system]/Stadus_Scoreboard/fxmanifest.lua
@@ -0,0 +1,15 @@
+
+fx_version 'bodacious'
+games { 'gta5' }
+
+server_script 'server/main.lua'
+
+client_script 'client/main.lua'
+
+ui_page 'html/scoreboard.html'
+
+files {
+	'html/scoreboard.html',
+	'html/style.css',
+	'html/listener.js'
+}
