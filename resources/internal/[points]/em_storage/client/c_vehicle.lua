cda1138b548013a8c80904b488f1e451ddd0fd30
Emma Davenport <davenport.physics@gmail.com>
2021-04-29 15:11:37 -0500

    Support for using F2 to open trunk and glovebox inventory

--- resources/internal/[points]/em_storage/client/c_vehicle.lua
+++ resources/internal/[points]/em_storage/client/c_vehicle.lua
@@ -1,47 +1,42 @@
 
-
-exports["em_commands"]:register_command_no_perms("open_glovebox", function(source, args, raw_command)
+local function attempt_open_glovebox_inventory()
 
     local ped     = PlayerPedId()
     local vehicle = GetVehiclePedIsIn(ped, false)
 
     if vehicle == 0 then
-        exports['t-notify']:Alert({style = 'error', message = "Not in a vehicle"})
-        return
+        return false
     end
 
     local ped_in_driver = GetPedInVehicleSeat(vehicle, -1)
     local ped_in_front_passenger = GetPedInVehicleSeat(vehicle, 0)
 
     if ped ~= ped_in_driver and ped ~= ped_in_front_passenger then
-        return
+        return false
     end
 
     local storage_id = exports["em_fw"]:get_vehicle_storage_id(GetVehicleNumberPlateText(vehicle), "Glovebox")
     TriggerEvent("esx_inventoryhud:open_secondary_inventory", storage_id, "Glovebox")
 
-end)
+end
 
-exports["em_commands"]:register_command_no_perms("open_trunk", function(source, args, raw_command)
+local function attempt_open_trunk_inventory()
 
     local ped = PlayerPedId()
 
     if GetVehiclePedIsIn(ped, false) ~= 0 then
-        exports['t-notify']:Alert({style = 'error', message = "Cannot be in a vehicle"})
         return
     end
 
     local vehicle = exports["em_fw"]:get_nearest_vehicle()
 
     if vehicle == 0 then
-        exports['t-notify']:Alert({style = 'error', message = "Vehicle not found"})
         return
     end
 
     local ped_coords = GetEntityCoords(ped)
     local veh_coords = GetEntityCoords(vehicle)
     if #(ped_coords - veh_coords) > 5.0 then
-        exports['t-notify']:Alert({style = 'error', message = "Too far away from a car"})
         return
     end
 
@@ -49,8 +44,17 @@ exports["em_commands"]:register_command_no_perms("open_trunk", function(source, 
         local storage_id = exports["em_fw"]:get_vehicle_storage_id(GetVehicleNumberPlateText(vehicle), "Trunk")
         TriggerEvent("esx_inventoryhud:open_secondary_inventory", storage_id, "Trunk")
     else
-        exports['t-notify']:Alert({style = 'error', message = "Trunk not open"})
         return
     end
 
-end)
+end
+
+RegisterCommand('secondary_inventory', function()
+
+    if attempt_open_glovebox_inventory() then
+        return
+    end
+    attempt_open_trunk_inventory()
+
+end, false)
+RegisterKeyMapping('secondary_inventory', 'secondary_inventory', 'keyboard', 'F2')
