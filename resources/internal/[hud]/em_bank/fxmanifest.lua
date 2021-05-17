e9aa5f7e9ce97c4d746bfc583a48745ef6d9ffd4
Emma Davenport <davenport.physics@gmail.com>
2021-05-17 10:44:52 -0500

    Initial commit of em_bank

---
+++ resources/internal/[hud]/em_bank/fxmanifest.lua
@@ -0,0 +1,24 @@
+
+fx_version 'bodacious'
+games { 'gta5' }
+
+client_scripts {
+    'client/c_bank.lua'
+}
+
+ui_page {
+    'html/ui.html',
+}
+
+files {
+    'html/ui.html',
+    'html/scripts/bank.js',
+    'html/css/bank.css',
+    'html/css/bulma/css/bulma.min.css'
+}
+
+dependencies {
+    
+    'em_dal'
+
+}
