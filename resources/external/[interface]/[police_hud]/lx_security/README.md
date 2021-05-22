d4b6b6665a48489021f6ceb9a913fa5ac61dfb23
Emma Davenport <davenport.physics@gmail.com>
2021-05-22 13:54:20 -0500

    Addition: cctv cameras

---
+++ resources/external/[interface]/[police_hud]/lx_security/README.md
@@ -0,0 +1,21 @@
+### 📷 [LX Security] - Support will only be provided in our Discord below.
+
+[![Discord](https://i.gyazo.com/c018365acc2739a9341cf46a406bcf36.png)](https://discord.gg/hPcvqtr)
+<hr>
+
+**LICENSE:** [Please read the license before using this.]
+
+<a rel="license" href="https://www.gnu.org/licenses/gpl-3.0.en.htm"><img alt="GNU General Public License 3.0" style="border-width:0" src="https://www.gnu.org/graphics/gplv3-127x51.png" /></a><br />
+
+This work is licensed under a <a rel="license" href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU General Public License 3.0</a>.
+<hr>
+
+**Features:**
+- Camera menu with categories, to view all of the cameras at a specific location.
+- Aces Support.
+- Export so you can get the closest camera to you, which can be implemented into other systems, such as dispatch alerts.
+- Several camera features such as rotating, zooming, and location cycling.
+- Config options to disable the minimap & your server's HUD when using a camera.
+
+**Dependencies:**
+xMenu by Xander1998 - https://github.com/xander1998/xmenu
---
+++ resources/external/[interface]/[police_hud]/lx_security/client/managers/camera.lua
@@ -0,0 +1,258 @@
+local selectedLocation = nil
+local selectedCamera = nil
+AddedLocations = {}
+scaleformState = "UNLOADED"
+changedCamera = false
+
+-- Commands
+exports["em_commands"]:register_command("cctv", function(source, args, raw)
+	local ped = PlayerPedId()
+	if Utils.HasPermission() then
+		AddedLocations = {}
+		local CameraMenu = xMenu.New("LX Security")
+		local GenStoreMenu = CameraMenu:BindSubMenu("General Stores")
+		local EntertainmentMenu = CameraMenu:BindSubMenu("Entertainment")
+		local BankMenu = CameraMenu:BindSubMenu("Banks")
+		local HospitalMenu = CameraMenu:BindSubMenu("Hospitals")
+		local PoliceStationsMenu = CameraMenu:BindSubMenu("Police Stations")
+
+		for k, v in pairs(CCTV_Config.Cameras) do
+			if v.type == "General Stores" then
+				if AddedLocations[v.name] == nil then
+					AddedLocations[v.name] = GenStoreMenu:BindSubMenu(v.name)
+				end
+
+				if AddedLocations[v.name] then
+					AddedLocations[v.name]:BindButton(v.name .. " #" .. v.camera, function()
+						selectedLocation = v.name
+						selectedCamera = v.camera
+						currentCamIndex = k
+						Utils.RequestCamera(v.name, v.camera)
+					end)
+				end
+			end
+			
+			if v.type == "Entertainment" then
+				if AddedLocations[v.name] == nil then
+					AddedLocations[v.name] = EntertainmentMenu:BindSubMenu(v.name)
+				end
+
+				if AddedLocations[v.name] then
+					AddedLocations[v.name]:BindButton(v.name .. " #" .. v.camera, function()
+						selectedLocation = v.name
+						selectedCamera = v.camera
+						currentCamIndex = k
+						Utils.RequestCamera(v.name, v.camera)
+					end)
+				end
+			end
+
+			if v.type == "Banks" then
+				if AddedLocations[v.name] == nil then
+					AddedLocations[v.name] = BankMenu:BindSubMenu(v.name)
+				end
+
+				if AddedLocations[v.name] then
+					AddedLocations[v.name]:BindButton(v.name .. " #" .. v.camera, function()
+						selectedLocation = v.name
+						selectedCamera = v.camera
+						currentCamIndex = k
+						Utils.RequestCamera(v.name, v.camera)
+					end)
+				end
+			end
+
+			if v.type == "Hospitals" then
+				if AddedLocations[v.name] == nil then
+					AddedLocations[v.name] = HospitalMenu:BindSubMenu(v.name)
+				end
+
+				if AddedLocations[v.name] then
+					AddedLocations[v.name]:BindButton(v.name .. " #" .. v.camera, function()
+						selectedLocation = v.name
+						selectedCamera = v.camera
+						currentCamIndex = k
+						Utils.RequestCamera(v.name, v.camera)
+					end)
+				end
+			end
+
+			if v.type == "Police Stations" then
+				if AddedLocations[v.name] == nil then
+					AddedLocations[v.name] = PoliceStationsMenu:BindSubMenu(v.name)
+				end
+
+				if AddedLocations[v.name] then
+					AddedLocations[v.name]:BindButton(v.name .. " #" .. v.camera, function()
+						selectedLocation = v.name
+						selectedCamera = v.camera
+						currentCamIndex = k
+						Utils.RequestCamera(v.name, v.camera)
+					end)
+				end
+			end
+		end
+
+		CameraMenu:OpenMenu()
+	else
+		if CCTV_Config.ShowNoPerms then
+			Utils.Notification("[~y~LX Security~w~]\n\n~r~You don't have permission to do this")
+		end
+	end
+end, "CCTV")
+
+currentCamIndex = 0
+switchingCam = false
+inCam = false
+cctvCam = 0
+cameraScaleform = nil
+controlsScaleform = nil
+camFov = 110.0
+local drawnControls = false
+
+local function back_space_check()
+	if IsDisabledControlJustPressed(0, 194) then -- Backspace
+		Utils.CloseCamera()
+		AddedLocations = {}
+		if CCTV_Config.HideRadar then
+			DisplayRadar(true)
+		end
+
+		if CCTV_Config.HideHUD then
+			Utils.ToggleHUD(true)
+		end
+	end
+end
+
+local function next_camera_check()
+	if IsDisabledControlJustPressed(0, 175) then -- Next Camera
+		switchingCam = true
+		local AMOUNT_OF_CAMERAS = Utils.GetCamAmountByName(AddedLocations[selectedLocation].Name)
+		if selectedCamera + 1 > AMOUNT_OF_CAMERAS then
+			selectedCamera = 1
+		else
+			selectedCamera = selectedCamera + 1
+		end
+
+		Utils.ChangeCamera(selectedLocation, selectedCamera)
+	end
+end
+
+local function camera_rotate_check()
+	if CCTV_Config.Cameras[currentCamIndex].canRotate then
+		local rotation = GetCamRot(cctvCam, 2)
+
+		if IsDisabledControlPressed(1, 108) then -- Num 4 (Rotate Left)
+			SetCamRot(cctvCam, rotation.x, 0.0, rotation.z + 0.3, 2)
+		--end
+		end
+
+		if IsDisabledControlPressed(1, 107) then -- Num 6 (Rotate Right)
+			SetCamRot(cctvCam, rotation.x, 0.0, rotation.z - 0.3, 2)
+		end
+
+		if IsDisabledControlPressed(1, 111) then -- Num 8 (Up)
+			if rotation.x <= 0.0 then
+				SetCamRot(cctvCam, rotation.x + 0.3, 0.0, rotation.z, 2)
+			end
+		end
+
+		if IsDisabledControlPressed(1, 110) then -- Num 5 (Down)
+			if rotation.x <= 50.0 and rotation.x >= -88.0 then
+				SetCamRot(cctvCam, rotation.x - 0.3, 0.0, rotation.z, 2)
+			end
+		end
+	end
+end
+
+local function previous_camera_check()
+	if IsDisabledControlJustPressed(0, 174) then -- Previous Camera
+		switchingCam = true
+		local AMOUNT_OF_CAMERAS = Utils.GetCamAmountByName(AddedLocations[selectedLocation].Name)
+		if selectedCamera - 1 <= 0 then
+			selectedCamera = AMOUNT_OF_CAMERAS
+		else
+			selectedCamera = selectedCamera - 1
+		end
+		
+		Utils.ChangeCamera(selectedLocation, selectedCamera)
+	end
+end
+
+local function zoom_in_check()
+	if IsDisabledControlJustPressed(0, 241) then -- Zoom In
+		if camFov > -1.0 then
+			camFov = camFov - 3.0
+			SetCamFov(cctvCam, camFov)
+		end
+	end
+end
+
+local function zoom_out_check()		
+	if IsDisabledControlJustPressed(0, 242) then -- Zoom Out
+		if camFov < 110.0 then
+			camFov = camFov + 3.0
+			SetCamFov(cctvCam, camFov)
+		end
+	end
+end
+
+local function scaleform_timecycle()
+	local location = CCTV_Config.Cameras[currentCamIndex].location
+	SetTimecycleModifier(CCTV_Config.TimecycleTypes[CCTV_Config.Cameras[currentCamIndex].cameraType])
+	SetTimecycleModifierStrength(1.0)
+	PushScaleformMovieFunction(cameraScaleform, "SET_ALT_FOV_HEADING")
+	-- PushScaleformMovieFunctionParameterFloat(GetEntityCoords(location.w).z)
+	PushScaleformMovieFunctionParameterFloat(location.w)
+	PushScaleformMovieFunctionParameterFloat(1.0)
+	PushScaleformMovieFunctionParameterFloat(GetCamRot(cctvCam, 2).z)
+	PopScaleformMovieFunctionVoid()
+	DrawScaleformMovieFullscreen(cameraScaleform, 255, 255, 255, 255)
+	DisableAllControlActions(0)
+end
+
+local function controls_scaleform()
+	if scaleformState == "CAMERA_READY" then
+		local buttonsMessage = {
+			{name = "Exit", button = 194},
+			{name = "Previous Camera", button = 174},
+			{name = "Next Camera", button = 175},
+			{name = "Zoom Out", button = 242},
+			{name = "Zoom In", button = 241},
+			{name = "Down", button = 112},
+			{name = "Up", button = 111},
+			{name = "Right", button = 109},
+			{name = "Left", button = 108}
+		}
+		controlsScaleform = Utils.CreateInstructions("instructional_buttons", buttonsMessage)
+		DrawScaleformMovieFullscreen(controlsScaleform, 255, 255, 255, 255, 0)
+	end
+end
+
+local in_loop = false
+
+function camera_loop()
+
+	if in_loop then
+		return
+	end
+
+	in_loop = true
+	while inCam do
+
+		Citizen.Wait(0)
+		back_space_check()
+
+		if currentCamIndex > 0 then
+			camera_rotate_check()
+			next_camera_check()
+			previous_camera_check()
+			zoom_in_check()
+			zoom_out_check()
+		end
+		scaleform_timecycle()
+		controls_scaleform()
+	end
+	in_loop = false
+
+end
