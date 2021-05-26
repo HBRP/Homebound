2d4b0eb400283c6cf3c38f0fec07700287863962
Emma Davenport <davenport.physics@gmail.com>
2021-05-25 20:12:10 -0500

    Full web implementation of form code

--- resources/internal/[hud]/em_form/html/scripts/form.js
+++ resources/internal/[hud]/em_form/html/scripts/form.js
@@ -1,10 +1,22 @@
 var current_form;
 var unique_id = 0;
 function add_dropdown_menu(idx, menu) {
-    $('.input-container').append("\n\t\t<div class=\"dropdown is-active\">\n\t\t  \t<div class=\"dropdown-trigger\">\n\t\t    \t<button class=\"button\" aria-haspopup=\"true\" aria-controls=\"dropdown-menu\">\n\t\t      \t<span>Dropdown button</span>\n\t\t      \t<span class=\"icon is-small\">\n\t\t        \t<i class=\"fas fa-angle-down\" aria-hidden=\"true\"></i>\n\t\t      \t</span>\n\t\t    \t</button>\n\t\t  \t</div>\n\t\t\t<div class=\"dropdown-menu input-" + idx + "\" role=\"menu\">\n\t\t\t    <div class=\"dropdown-content dropdown-content-" + idx + "\"></div>\n\t\t\t</div>\n\t\t</div>\n\t\t");
+    $('.input-container').append("\n\t\t<div class=\"dropdown dropdown-" + idx + "\">\n\t\t  \t<div class=\"dropdown-trigger\">\n\t\t    \t<button class=\"button\" aria-haspopup=\"true\" aria-controls=\"dropdown-menu\">\n\t\t      \t<span class='selected-input-" + idx + "'>" + menu.input_name + "</span>\n\t\t      \t<span class=\"icon is-small\">\n\t\t        \t<i class=\"fas fa-angle-down\" aria-hidden=\"true\"></i>\n\t\t      \t</span>\n\t\t    \t</button>\n\t\t  \t</div>\n\t\t\t<div class=\"dropdown-menu input-" + idx + "\" role=\"menu\">\n\t\t\t    <div class=\"dropdown-content dropdown-content-" + idx + "\"></div>\n\t\t\t</div>\n\t\t</div>\n\t\t");
     for (var i = 0; i < menu.options.length; i++) {
-        $(".dropdown-content-" + idx).append("<a class=\"dropdown-item\">" + menu.options[i] + "</a>");
+        if (i == 0) {
+            $(".dropdown-content-" + idx).append("<a class=\"dropdown-item dropdown-item-" + idx + " is-active\" style=\"text-align:left;\">" + menu.options[i] + "</a>");
+        }
+        else {
+            $(".dropdown-content-" + idx).append("<a class=\"dropdown-item dropdown-item-" + idx + "\" style=\"text-align:left;\">" + menu.options[i] + "</a>");
+        }
     }
+    $(".dropdown-" + idx).click(function () {
+        $(".dropdown-" + idx).toggleClass('is-active');
+    });
+    $(".dropdown-item-" + idx).click(function () {
+        $(".dropdown-item-" + idx).removeClass('is-active');
+        $(this).addClass('is-active');
+    });
 }
 function add_text_input(idx, input) {
     $('.input-container').append("\n\t\t<div class=\"field\">\n\t\t\t<label class=\"label\">" + input.input_name + "</label>\n\t\t\t<div class=\"control\">\n\t\t\t\t<input class=\"input input-" + idx + "\" placeholder=\"" + input.placeholder + "\">\n\t\t\t</div>\n\t\t</div>\t\n\t");
@@ -21,14 +33,67 @@ function generate_form(title, form) {
     }
     $('.form-title').html("<b>" + title + "</b>");
 }
-function submit() {
-    for (var i = 0; i < current_form.length; i++) {
-        if (current_form[i].input_type == "text_input") {
-        }
-        else if (current_form[i].input_type == "dropdown") {
+function parse_text_input(idx, form) {
+    var successful = false;
+    var input = $(".input-" + idx).val().toString();
+    if (!form.optional && input.length == 0) {
+        return [false, 0];
+    }
+    if (!form.numbers_valid) {
+        if (/\d/g.test(input)) {
+            return [false, 0];
         }
     }
-    //$('.form-container').hide();
+    if (!form.characters_valid) {
+        if (/[a-zA-Z]/g.test(input)) {
+            return [false, 0];
+        }
+    }
+    return [true, input];
+}
+function parse_dropdown_selection(idx, form) {
+    console.log($(".dropdown-item-" + idx));
+    for (var i = 0; i < form.options.length; i++) {
+        if ($(".dropdown-item-" + idx)[i].classList.contains("is-active")) {
+            return [true, $(".dropdown-item-" + idx)[i].innerText];
+        }
+    }
+    return [false, 0];
+}
+function submit() {
+    var found_problem = false;
+    var inputs;
+    for (var i = 0; i < current_form.length; i++) {
+        $(".input-" + i).removeClass('is-danger');
+        if (current_form[i].input_type == "text_input") {
+            var parsed = parse_text_input(i, current_form[i]);
+            if (!parsed[0]) {
+                $(".input-" + i).addClass('is-danger');
+                found_problem = true;
+                continue;
+            }
+            inputs.push({
+                input_name: current_form[i].input_name,
+                value: parsed[1]
+            });
+        }
+        else if (current_form[i].input_type == "dropdown") {
+            var parsed = parse_dropdown_selection(i, current_form[i]);
+            if (!parsed[0]) {
+                found_problem = true;
+                continue;
+            }
+            inputs.push({
+                input_name: current_form[i].input_name,
+                value: parsed[1]
+            });
+        }
+    }
+    if (found_problem) {
+        return;
+    }
+    $('.form-container').hide();
+    $.post("http://em_form/submit", JSON.stringify(inputs));
 }
 function display() {
     $('.form-container').show();
@@ -41,7 +106,8 @@ function test_generate() {
             placeholder: "enter some text here",
             options: [],
             numbers_valid: true,
-            characters_valid: false
+            characters_valid: false,
+            optional: false
         },
         {
             input_type: "text_input",
@@ -49,7 +115,8 @@ function test_generate() {
             placeholder: "enter some text here",
             options: [],
             numbers_valid: false,
-            characters_valid: true
+            characters_valid: true,
+            optional: false
         },
         {
             input_type: "text_input",
@@ -57,15 +124,17 @@ function test_generate() {
             placeholder: "enter some text here",
             options: [],
             numbers_valid: true,
-            characters_valid: true
+            characters_valid: true,
+            optional: false
         },
         {
             input_type: "dropdown",
             input_name: "fourth_form",
             placeholder: "",
-            options: ["first option", "section option"],
+            options: ["first option", "second option", "third option"],
             numbers_valid: true,
-            characters_valid: true
+            characters_valid: true,
+            optional: false
         }
     ];
     $('.input-container').html('');
@@ -75,6 +144,7 @@ function test_generate() {
 $(function () {
     $('.cancel-button').click(function () {
         $('.form-container').hide();
+        $.post("http://em_form/quit", JSON.stringify({}));
     });
     $('.submit-button').click(function () {
         submit();
--- resources/internal/[hud]/em_form/html/scripts/form.ts
+++ resources/internal/[hud]/em_form/html/scripts/form.ts
@@ -7,6 +7,14 @@ interface FormInput {
 	options: string[],
 	numbers_valid: boolean,
 	characters_valid: boolean,
+	optional: boolean
+
+}
+
+interface ParsedInput {
+
+	input_name: string,
+	value: any
 
 }
 
@@ -17,10 +25,10 @@ function add_dropdown_menu(idx: number, menu: FormInput) {
 
 	$('.input-container').append(
 		`
-		<div class="dropdown is-active">
+		<div class="dropdown dropdown-${idx}">
 		  	<div class="dropdown-trigger">
 		    	<button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-		      	<span>Dropdown button</span>
+		      	<span class='selected-input-${idx}'>${menu.input_name}</span>
 		      	<span class="icon is-small">
 		        	<i class="fas fa-angle-down" aria-hidden="true"></i>
 		      	</span>
@@ -30,13 +38,34 @@ function add_dropdown_menu(idx: number, menu: FormInput) {
 			    <div class="dropdown-content dropdown-content-${idx}"></div>
 			</div>
 		</div>
-		`)
+		`);
 
 	for (var i = 0;i < menu.options.length; i++) {
 
-		$(`.dropdown-content-${idx}`).append(`<a class="dropdown-item">${menu.options[i]}</a>`)
+		if (i == 0) {
+
+			$(`.dropdown-content-${idx}`).append(`<a class="dropdown-item dropdown-item-${idx} is-active" style="text-align:left;">${menu.options[i]}</a>`);
+
+		} else {
+
+			$(`.dropdown-content-${idx}`).append(`<a class="dropdown-item dropdown-item-${idx}" style="text-align:left;">${menu.options[i]}</a>`);
+
+		}
 
 	}
+
+	$(`.dropdown-${idx}`).click(function() {
+
+		$(`.dropdown-${idx}`).toggleClass('is-active');
+
+	})
+
+	$(`.dropdown-item-${idx}`).click(function() {
+
+		$(`.dropdown-item-${idx}`).removeClass('is-active');
+		$(this).addClass('is-active');
+
+	})
 
 
 }
@@ -75,22 +104,107 @@ function generate_form(title: string, form: FormInput[]) {
 
 }
 
-function submit() {
+function parse_text_input(idx: number, form: FormInput) : [boolean, any] {
 
-	for (var i = 0; i < current_form.length; i++) {
+	let successful = false;
+	let input: string = $(`.input-${idx}`).val().toString();
 
-		if (current_form[i].input_type == "text_input") {
+	if (!form.optional && input.length == 0) {
 
+		return [false, 0];
 
-		} else if (current_form[i].input_type == "dropdown") {
+	}
 
+	if (!form.numbers_valid) {
 
+		if (/\d/g.test(input)) {
+
+			return [false, 0];
 
 		}
 
 	}
 
-	//$('.form-container').hide();
+	if (!form.characters_valid) {
+
+		if (/[a-zA-Z]/g.test(input)) {
+
+			return [false, 0];
+
+		}
+
+	}
+
+	return [true, input];
+
+
+}
+
+function parse_dropdown_selection(idx: number, form: FormInput) : [boolean, any] {
+
+	console.log($(`.dropdown-item-${idx}`))
+	for (var i = 0;i < form.options.length;i++) {
+
+		 if ($(`.dropdown-item-${idx}`)[i].classList.contains("is-active")) {
+
+		 	return [true, $(`.dropdown-item-${idx}`)[i].innerText]
+
+		 }
+
+	}
+
+	return [false, 0];
+
+}
+
+function submit() {
+
+	let found_problem = false;
+	let inputs: ParsedInput[];
+	for (var i = 0; i < current_form.length; i++) {
+
+		$(`.input-${i}`).removeClass('is-danger');
+		if (current_form[i].input_type == "text_input") {
+
+			let parsed = parse_text_input(i, current_form[i]);
+
+			if (!parsed[0]) {
+
+				$(`.input-${i}`).addClass('is-danger');
+				found_problem = true;
+				continue;
+
+			}
+			inputs.push({
+				input_name: current_form[i].input_name,
+				value: parsed[1]
+			})
+
+
+		} else if (current_form[i].input_type == "dropdown") {
+
+			let parsed = parse_dropdown_selection(i, current_form[i]);
+			if (!parsed[0]) {
+
+				found_problem = true;
+				continue;
+
+			}
+			inputs.push({
+				input_name: current_form[i].input_name,
+				value: parsed[1]
+			})
+
+		}
+
+	}
+
+	if (found_problem) {
+		return;
+	}
+
+	$('.form-container').hide();
+	$.post("http://em_form/submit", JSON.stringify(inputs));
 
 }
 
@@ -109,7 +223,8 @@ function test_generate() {
 			placeholder: "enter some text here",
 			options: [],
 			numbers_valid: true,
-			characters_valid: false
+			characters_valid: false,
+			optional: false
 		},
 		{
 			input_type: "text_input",
@@ -117,7 +232,8 @@ function test_generate() {
 			placeholder: "enter some text here",
 			options: [],
 			numbers_valid: false,
-			characters_valid: true
+			characters_valid: true,
+			optional: false
 		},
 		{
 			input_type: "text_input",
@@ -125,15 +241,17 @@ function test_generate() {
 			placeholder: "enter some text here",
 			options: [],
 			numbers_valid: true,
-			characters_valid: true
+			characters_valid: true,
+			optional: false
 		},
 		{
 			input_type: "dropdown",
 			input_name: "fourth_form",
 			placeholder: "",
-			options: ["first option", "section option"],
+			options: ["first option", "second option", "third option"],
 			numbers_valid: true,
-			characters_valid: true
+			characters_valid: true,
+			optional: false
 		}
 	]
 
@@ -149,6 +267,7 @@ $(function() {
 	$('.cancel-button').click(function() {
 
 		$('.form-container').hide();
+		$.post("http://em_form/quit", JSON.stringify({}));
 
 	})
 
