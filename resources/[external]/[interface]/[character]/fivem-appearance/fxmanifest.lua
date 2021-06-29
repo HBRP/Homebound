9458cae22e98bf30b09fc2361093fb0e5208a879
Emma Davenport <davenport.physics@gmail.com>
2021-06-29 17:32:08 -0500

    Updated fivem-appearance to 2.2.1

--- resources/[external]/[interface]/[character]/fivem-appearance/fxmanifest.lua
+++ resources/[external]/[interface]/[character]/fivem-appearance/fxmanifest.lua
@@ -4,15 +4,15 @@ game { "gta5" }
 author 'Snake'
 description 'A flexible player customization script for FiveM.'
 repository 'https://github.com/snakewiz/fivem-appearance'
-version '1.1.1'
+version '1.2.0'
 
 client_script 'typescript/build/client.js'
 
 files {
-    'peds.json',
-    'ui/build/index.html',
-    'ui/build/static/js/*.js',
-    'locales/*.json',
+  'ui/build/index.html',
+  'ui/build/static/js/*.js',
+  'locales/*.json',
+  'peds.json'
 }
 
 ui_page 'ui/build/index.html'
