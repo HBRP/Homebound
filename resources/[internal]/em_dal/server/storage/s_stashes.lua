c0e8fdcc98e6b69bb6b545c060b3a046a4f34333
Emma Davenport <davenport.physics@gmail.com>
2021-07-16 16:39:55 -0500

    Change: send character_id to rusty-m, which gives support for group storage containers

--- resources/[internal]/em_dal/server/storage/s_stashes.lua
+++ resources/[internal]/em_dal/server/storage/s_stashes.lua
@@ -1,7 +1,8 @@
 register_server_callback("em_dal:get_nearby_stashes", function(source, callback, x, y, z)
 
 
-    local endpoint = string.format("/Storage/NearbyStashes/%.4f/%.4f/%.4f", x, y, z)
+    local character_id = get_character_id_from_source(source)
+    local endpoint = string.format("/Storage/NearbyStashes/%d/%.4f/%.4f/%.4f", character_id, x, y, z)
     HttpGet(endpoint, nil, function(error_code, result_data, result_headers)
 
         local temp = json.decode(result_data)
