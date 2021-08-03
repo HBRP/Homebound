aa559a026f8785697ab61ffffe506ddd2359652c
Emma Davenport <davenport.physics@gmail.com>
2021-08-03 14:18:28 -0500

    Change: Initialize vehicle shop menu at resource start

--- resources/[internal]/[gameplay]/em_vehicle_shop/client/em_vehicle_shop.lua
+++ resources/[internal]/[gameplay]/em_vehicle_shop/client/em_vehicle_shop.lua
@@ -14,23 +14,33 @@ local categories ={
     'bicycles'
 }
 
-local function open_vehicle_shop_menu()
+local vehicle_shop_menu = nil
+local category_menus = {}
 
-	local menu = MenuV:CreateMenu('Vehicles', 'Select Category', 'topleft', 0, 0, 255, 'size-125', 'default', 'menuv', 'vehicle_shop', 'native')
-	menu:ClearItems()
+local function setup_menus()
+
+    vehicle_shop_menu = MenuV:CreateMenu('Vehicles', 'Select Category', 'topleft', 0, 0, 255, 'size-125', 'default', 'menuv', 'vehicle_shop', 'native')
+    for i = 1, #categories do
+        category_menus[i] = MenuV:CreateMenu(categories[i], 'Pull out vehicle', 'topleft', 0, 0, 255, 'size-125', 'default', 'menuv', categories[i], 'native')
+    end
+
+end
+
+local function open_vehicle_shop_menu(stock)
+
+    vehicle_shop_menu:ClearItems()
 
 	for i = 1, #categories do
 
-		local category_menu = MenuV:CreateMenu(categories[i], 'Pull out vehicle', 'topleft', 0, 0, 255, 'size-125', 'default', 'menuv', categories[i], 'native')
-		local button = menu:AddButton({label = categories[i], value = category_menu})
+		local button = vehicle_shop_menu:AddButton({label = categories[i], value = category_menus[i]})
 
-		category_menu:On('open', function(m)
+		category_menus[i]:On('open', function(m)
+            category_menus[i]:ClearItems()
 			m:AddButton({label = 'temp', value = function() end})
 		end)
 
 	end
-    MenuV:OpenMenu(menu)
-
+    MenuV:OpenMenu(vehicle_shop_menu)
 
 end
 
@@ -40,10 +50,24 @@ exports["em_context"]:register_context("PDM Vehicle Floor", function()
         dialog = "Pull out vehicle",
         callback = function()
 
-            exports["em_dialog"]:hide_dialog() 
-            open_vehicle_shop_menu()
+            exports["em_dialog"]:hide_dialog()
+            exports["em_dal"]:get_vehicle_store_stock_async(function(stock)
+
+                open_vehicle_shop_menu(stock)
+
+            end, "PDM Basic")
 
         end
     }
+
+end)
+
+Citizen.CreateThread(function()
+
+    Citizen.Wait(0)
+    setup_menus()
+
+    assert(vehicle_shop_menu ~= nil, "Vehicle shop menu could not be initialized")
+    assert(#category_menus > 0, "Vehicle category menus not initialized")
 
 end)
