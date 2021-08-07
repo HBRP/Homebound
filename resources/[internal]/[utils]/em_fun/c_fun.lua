069a215039e790cd3b85706ff7d795e0fe0de7d9
Emma Davenport <davenport.physics@gmail.com>
2021-08-07 09:31:08 -0500

    Addition: new functional library utils

---
+++ resources/[internal]/[utils]/em_fun/c_fun.lua
@@ -0,0 +1,70 @@
+local DATA_TYPE = {
+	CONTIGUOUS = 1,
+	NON_STANDARD = 2
+}
+
+
+Fun = {}
+Fun.__index = Fun
+
+function Fun:new(data)
+
+	local obj = {}
+	setmetatable(obj, Fun)
+	obj.data = data
+
+	obj.data_type = DATA_TYPE.CONTIGUOUS
+	for k, v in pairs(data) do
+		if type(k) ~= "number" then
+			obj.data_type = DATA_TYPE.NON_STANDARD
+		end
+		break
+	end
+
+	return obj
+
+end
+
+function Fun:map(func)
+
+	local data = {}
+	for k, v in pairs(self.data) do
+
+		data[k] = func(v)
+
+	end
+	return Fun:new(data)
+
+end
+
+function Fun:filter(func)
+
+	local data = {}
+
+	if self.data_type == DATA_TYPE.NON_STANDARD then
+		for k, v in pairs(self.data) do
+
+			if func(v) then
+				data[k] = v
+			end
+
+		end
+	else
+		for k, v in pairs(self.data) do
+
+			if func(v) then
+				data[#data + 1] = v
+			end
+
+		end
+	end
+
+	return Fun:new(data)
+
+end
+
+function Fun:into_inner()
+
+	return self.data
+
+end
---
+++ resources/[internal]/[utils]/em_fun/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+client_scripts {
+	'c_fun.lua'
+}
